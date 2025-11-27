b7f958f5d29758477b362430df3a4192220ecb61
Rohit Mahor <rohitmaho7678@gmail.com>
2025-11-27 04:55:30 -0800

    add create-admin endpoint

--- ai_prompt_hub/urls.py
+++ ai_prompt_hub/urls.py
@@ -10,7 +10,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-# 🔥 TEMPORARY MIGRATION ENDPOINT (will remove after use)
+# FOR TEMP MIGRATION
 from django.core.management import call_command
 from django.http import JsonResponse
 
@@ -21,19 +21,39 @@ def run_migrations(request):
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
 
+# 🔥 TEMP ADMIN CREATION
+from django.contrib.auth.models import User
+
+def create_admin(request):
+    try:
+        if User.objects.filter(username="admin").exists():
+            return JsonResponse({"status": "admin already exists"})
+        
+        User.objects.create_superuser(
+            username="rohit",
+            password="rohit7678@",
+            email="admin@example.com"
+        )
+        return JsonResponse({"status": "admin user created"})
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # API
     path('api/', include('prompts_app.urls')),
-    
+
     # JWT Login Endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # 🔥 Temporary migration URL
+    # Temporary migration URL
     path('run-migrations/', run_migrations),
+
+    # Temporary admin creation URL
+    path('create-admin/', create_admin),
 ]
 
 # Serve media files in development
